b3ba149358ad777f85654aa84f2055c83827ecc9
mihaiconstantin <c.tinmihai@ymail.com>
2018-07-10 00:22:08 +0200

    Added `tryCatch()` when running a cell to prevent execution halt

--- R/procedure.R
+++ R/procedure.R
@@ -1,19 +1,12 @@
 # This file contains the functions needed for running and replicating the procedure.
 
-# TODO:
-# 1. fix the feedback test (~)
-# 2. make function to run portions of the design on different PCs (~ ~)
-# 3. look for option to use multicore package (~ ~)
-# 4. create class methods for 'netPowerCell', 'netPowerCells', 'netPowerReplication'
-# 	- print and plot
-
 
 
 #' @title .
 #' @export
 run_cell <- function(participants, nodes, architecture, connectedness, model) {
 	# User feedback at start.
-	cat('\t-> cell configuration:', participants, 'participants |', nodes, 'nodes |', architecture, 'architecture |', connectedness, 'connectedness |', model, 'model. ')
+	cat('\t-> config:', participants, 'par |', nodes, 'nod |', architecture, 'arc |', connectedness, 'con |', model, 'mod. ')
 	
 	# Select the true model.
 	true_model <- select_true_model(nodes, architecture, connectedness, model)
@@ -29,7 +22,7 @@ run_cell <- function(participants, nodes, architecture, connectedness, model) {
 	class(result) <- c('netPowerCell', 'list')
 
 	# User feedback at the end.
-	cat('Cell done \u2713. \n')
+	cat('Cell done. \u2713\n')
 
 	return(result)
 }
@@ -48,7 +41,24 @@ run_cells <- function(cells) {
 	# Running the cells.
 	for (cell in 1:nrow(cells))
 	{
-		cell_result = run_cell(cells[cell, 1], cells[cell, 2], cells[cell, 3], cells[cell, 4], cells[cell, 5])
+		cell_result = tryCatch(
+			{
+				# Try to run the cell.
+				run_cell(cells[cell, 1], cells[cell, 2], cells[cell, 3], cells[cell, 4], cells[cell, 5])
+			}, 
+			error = function(error) {
+				# Provide feedback in the console.
+				cat('Cell error. Skipped. \n')
+
+				# Inform about the error (i.e., for logging.)
+				info = paste0('Simulation error at index ', cell, ' in run_cells(). Cell config: ', cells[cell, 1], ' ', cells[cell, 2], ' ', cells[cell, 3], ' ', cells[cell, 4], ' ', cells[cell, 5], '.')
+				message(paste0(cat('\t'), 'SIMERR: ', Sys.time(), '. ', info))
+				message(paste0(cat('\t'), 'Actual error: ', error))
+				
+				# Return the info string to the results object.
+				return(info)
+			}
+		)
 		results[[cell]] = cell_result
 	}
 	
@@ -67,7 +77,7 @@ run_cells <- function(cells) {
 #' @export
 replicate_cells <- function(cells, replications) {
 	# User feedback at start.
-	cat('Design replications requested:', replications, '.\n', sep = '')
+	cat('Design replications requested: ', replications, '.\n', sep = '')
 
 	# Storing the results per replication (i.e., each replication contains the results of each design cell).
 	results = list()
@@ -87,7 +97,7 @@ replicate_cells <- function(cells, replications) {
 	}
 
 	# Set the class to allow for fancier handling of the nested data structure.
-	class(results) <- class('netPowerReplication', 'list')
+	class(results) <- c('netPowerReplication', 'list')
 
 	# User feedback at end.
 	cat('\nCompleted all', replications, 'replications.\n\n')
