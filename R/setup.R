36ceadf12670a1e7f4c41c8cc9476044fe0eff1c
mihaiconstantin <c.tinmihai@ymail.com>
2018-07-10 02:56:17 +0200

    Improved `run_cell_range()`

--- R/setup.R
+++ R/setup.R
@@ -5,7 +5,7 @@
 #' @title . 
 #' @export
 initialize_design <- function() {
-	participants 	= seq(50, 1000, 50)
+	participants 	= seq(100, 1000, 100)
 	nodes 			= c(10, 20, 30)
 	architectures 	= c(random = 1, small_world = 2, scale_free = 3)
 	connectedness 	= c(low = 1, medium = 2, large = 3)
@@ -24,13 +24,18 @@ initialize_design <- function() {
 
 #' @title .
 #' @export
-run_cell_range <- function(start, end, directory = ".", replications = 100)
-{
+run_cell_range <- function(start, end, directory = ".", replications = 100, console) {
+	# Starting time.
+	t0 = Sys.time()
+
+	# Create a unique identifier for the data.
+	uuid = paste0(as.numeric(Sys.time()), '_', console, '_', round(runif(1), 5), '_')
+
 	# Set the working directory.
 	setwd(directory)
 
 	# Open the connection to the log file.
-	logs <- file(paste0(start, "_to_", end, "_logs.Rlog"), open = "wt")
+	logs <- file(paste0(uuid, start, "_to_", end, "_logs.Rlog"), open = "wt")
 
 	# Store all incoming messages, warnings, and errors.
 	sink(logs, type = "message")
@@ -40,11 +45,15 @@ run_cell_range <- function(start, end, directory = ".", replications = 100)
 	results = replicate_cells(designs[start:end, ], replications)
 	
 	# Store the results.
-	saveRDS(object = results, 				file = paste0(start, "_to_", end, "_results.RData"))
-	saveRDS(object = designs[start:end, ], 	file = paste0(start, "_to_", end, "_designs.RData"))
+	saveRDS(object = results, 				file = paste0(uuid, start, "_to_", end, "_results.RData"))
+	saveRDS(object = designs[start:end, ], 	file = paste0(uuid, start, "_to_", end, "_designs.RData"))
+
+	# Ending time.
+	t1 = Sys.time()
 
 	# Notify that everything went fine.
-	cat(paste0(start, "_to_", end, "_results.RData"), "was sucessfuly written at", directory, "\n\n")
+	cat(paste0(uuid, start, "_to_", end, "_results.RData"), "was sucessfuly written at", directory, "\n\n")
+	cat(paste0('Start: ', t0, '. End: ', t1, '. Delta: ', t1 - t0, '.'))
 
 	# Close the connection to the log file and disable the sink.
 	sink(type = "message")
