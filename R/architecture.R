07d5efb1b4d58d9036f29575e20f8a3d6637dbeb
mihaiconstantin <c.tinmihai@ymail.com>
2018-12-10 13:59:41 +0100

    Refactored the functions in the `architecture.R` file

--- R/architecture.R
+++ R/architecture.R
@@ -3,12 +3,30 @@
 
 
 
-#' @title .
-#' @export
-architecture_random <- function(nodes, ...) {
+architecture.random <- function(nodes, p) {
 	# Graph.
-	graph <- as.matrix(igraph::get.adjacency(igraph::erdos.renyi.game(nodes, ...)))
+	graph <- as.matrix(igraph::get.adjacency(igraph::erdos.renyi.game(nodes, p)))
 	
+	# Return graph.
+	return(graph)
+}
+
+
+
+architecture.small.world <- function(nodes, neighborhood, p) {
+	# Graph.
+	graph <- as.matrix(igraph::get.adjacency(igraph::sample_smallworld(1, nodes, neighborhood, p)))
+	
+	# Return graph.
+	return(graph)
+}
+
+
+
+architecture.scale.free <- function(nodes, attachment, edges) {
+	# Graph.
+	graph <- as.matrix(igraph::get.adjacency(igraph::sample_pa(nodes, power = attachment, m = edges, directed = F)))
+
 	# Return graph.
 	return(graph)
 }
@@ -17,22 +35,46 @@ architecture_random <- function(nodes, ...) {
 
 #' @title .
 #' @export
-architecture_small_world <- function(nodes, ...) {
-	# Graph.
-	graph <- as.matrix(igraph::get.adjacency(igraph::sample_smallworld(1, nodes, ...)))
-	
-	# Return graph.
-	return(graph)
-}
+get.architecture <- function(type, nodes, ...) {
+	# Capture the dot arguments.
+	. <- list(...)
+
+	# Make sure that the dots are not empty.
+	if(length(.) == 0) {
+		stop("Invalid `...` arguments. Please check the documentation.")
+	}
+
+	# Providing a random graph.
+	if(type == "random") {
+		# Stop the execution if the correct arguments are not specified.
+		if(is.null(.[["p"]])) stop("Missing expected argument(s). See the documentation.")
+
+		# Provide the graph.
+		return(
+			architecture.random(nodes, .[["p"]])
+		)
+	}
+
+	# Providing a small world graph.
+	if(type == "smallworld") {
+		# Stop the execution if the correct arguments are not specified.
+		if(is.null(.[["neighborhood"]]) || is.null(.[["p"]])) stop("Missing expected argument(s). See the documentation.")
+
+		# Provide the graph.
+		return(
+			architecture.small.world(nodes, .[["neighborhood"]], .[["p"]])
+		)
+	}
 
 
+	# Providing a scale free graph. 
+	if(type == "scalefree") {
+		# Stop the execution if the correct arguments are not specified.
+		if(is.null(.[["attachment"]]) || is.null(.[["edges"]])) stop("Missing expected argument(s). See the documentation.")
 
-#' @title .
-#' @export
-architecture_scale_free <- function(nodes, ...) {
-	# Graph.
-	graph <- as.matrix(igraph::get.adjacency(igraph::sample_pa(nodes, ..., directed = F)))
-
-	# Return graph.
-	return(graph)
+		# Provide the graph.
+		return(
+			architecture.scale.free(nodes, .[["attachment"]], .[["edges"]])
+		)
+	}
 }
