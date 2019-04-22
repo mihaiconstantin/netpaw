e4fbcda22b23f590744edb82e707d1db9c210601
mihaiconstantin <c.tinmihai@ymail.com>
2019-04-22 21:40:38 +0200

    Added test to check that `gen.graph` uses the correct graph generator

--- tests/testthat/test_graph_generation.R
+++ tests/testthat/test_graph_generation.R
@@ -7,3 +7,15 @@ test_that("`gen.graph` inherits from the correct class", {
     
     expect_is(true.graph, "npgraph")
 })
+
+
+
+test_that("`gen.graph` uses the correct graph generator", {
+    expect_equal(gen.graph("random", 5, p = .5)$type, "random")
+
+    expect_equal(gen.graph("smallworld", 5, neighborhood = 2, p = .5)$type, "smallworld")
+    
+    expect_equal(gen.graph("scalefree", 5, edges = 2, attachment = 2)$type, "scalefree")
+    
+    expect_error(gen.graph("unknown", "random", 5, p = .5)$type, "Unsupported graph type. Please request it at `m.a.constantin@uvt.nl`.")
+})
