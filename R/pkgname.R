df4c3a2f1e0fe01759b9828764f1909a89453390
mihaiconstantin <c.tinmihai@ymail.com>
2019-04-22 22:12:18 +0200

    Updated the notes for my future self

--- R/pkgname.R
+++ R/pkgname.R
@@ -39,6 +39,6 @@
 #     4. estimation.R -> fits a model to a give data object
 #     5. outcome.R -> compares a true model and an estimated model and computes several outcome indicators
 #     6. simulation.R -> a wrapper around the files from 1 to 5 that allows to replicate and store results
-#     7. utils.R -> contains useless functions that I once thought were useful
-#     8. externals.R -> contains code mostly copied---and simplified a bit---from other packages
-#     9. post.R -> allows to post the result of file 5 to whatever API endpoint
+#     7. post.R -> allows to post the result of file 5 to whatever API endpoint
+#     8. utils.R -> contains useless functions that I once thought were useful
+#     9. externals.R -> contains code mostly copied---and simplified a bit---from other packages
