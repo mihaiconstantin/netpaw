225cde70b62dca07a61b11bef587ac1012fa8571
mihaiconstantin <c.tinmihai@ymail.com>
2019-04-30 12:53:21 +0200

    Added file to store constants used throughout the package

---
+++ R/011_constants.R
@@ -0,0 +1,6 @@
+# Constants ---------------------------------------------------------------
+POSITIVE.EDGE.COLOR = "#3F51B5"
+NEGATIVE.EDGE.COLOR = "#F44336"
+
+SUPPORTED.MODELS <- c("ggm", "ising")
+SUPPORTED.GRAPHS <- c("random", "smallworld", "scalefree")
