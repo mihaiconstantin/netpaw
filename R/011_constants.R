e5001a98a3fbcea7530e66e958b982552444c6a3
mihaiconstantin <c.tinmihai@ymail.com>
2019-05-16 20:49:50 +0200

    Removed old constants file

--- R/011_constants.R
+++
@@ -1,6 +0,0 @@
-# Constants ---------------------------------------------------------------
-POSITIVE.EDGE.COLOR = "#3F51B5"
-NEGATIVE.EDGE.COLOR = "#F44336"
-
-SUPPORTED.MODELS <- c("ggm", "ising")
-SUPPORTED.GRAPHS <- c("random", "smallworld", "scalefree")
