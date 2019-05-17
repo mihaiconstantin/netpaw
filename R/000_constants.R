40cb716f9911fdd3427fb811f5a66cc4d3c8a897
mihaiconstantin <c.tinmihai@ymail.com>
2019-05-17 15:06:03 +0200

    Added `CLASSES` constant to keep track of active class names in package

--- R/000_constants.R
+++ R/000_constants.R
@@ -2,6 +2,41 @@
 
 
 
+# Package classes  --------------------------------------------------------
+CLASSES <- list(
+    graph = c(
+        "np.graph" # Information about the generated unweighted graph.
+    ),
+    
+    model = c(
+        "np.cs.model", # True model for cross-sectional data. 
+        "np.ts.model", # True model for time-series data.
+        "np.cs.fit",   # Estimated model for cross-sectional data.
+        "np.ts.fit"    # Estimated model for time-series data.
+    ),
+
+    data = c(
+        "np.data" # Information about the generated data.
+    ),
+
+    outcome = c(
+        "np.cs.out", # Outcome from comparison between true and fit (i.e., cross-sectional).
+        "np.ts.out"  # Outcome from comparison between true and fit (i.e., time-series).
+    ),
+
+    procedure = c(
+        "np.sim.cell", # Information about a single cell, ran only one time.
+        "np.sim.repl"  # Information about a single cell, ran multiple time (i.e., replicated).
+    ),
+
+    setup = c(
+        "np.config", # Information about the config of a single cell.
+        "np.design" # Information about the factorial design of design levels.
+    )
+)
+
+
+
 # Metadata ----------------------------------------------------------------
 
 POSITIVE.EDGE.COLOR = "#3F51B5"
