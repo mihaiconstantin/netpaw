4266536ee90fa8b0214cd24dc20e9b3ef0276a14
mihaiconstantin <c.tinmihai@ymail.com>
2019-05-24 14:20:25 +0200

    Reorganized `constants.R` and added `..ERRORS..` list for error messages

--- R/000_constants.R
+++ R/000_constants.R
@@ -1,11 +1,62 @@
-# This file holds constants used throughout the entire package.
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+#                                                             _                                                                           #
+#                                                            | |                                                                          #
+#                                                _ __    ___ | |_  _ __    __ _ __      __                                                #
+#                                               | '_ \  / _ \| __|| '_ \  / _` |\ \ /\ / /                                                #
+#                                               | | | ||  __/| |_ | |_) || (_| | \ V  V /                                                 #
+#                                               |_| |_| \___| \__|| .__/  \__,_|  \_/\_/                                                  #
+#                                                                 | |                                                                     #
+#                                                                 |_|                                                                     #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+#                                                                                                                                         #
+# File contributors:                                                                                                                      #
+#   - M.A. Constantin                                                                                                                     #
+#                                                                                                                                         #
+# File description:                                                                                                                       #
+#   - this file contains constants used throughout the entire package                                                                     #
+#                                                                                                                                         #
+# Classes/ functions:                                                                                                                     #
+#   - n.a.                                                                                                                                #
+#                                                                                                                                         #
+# Additional information:                                                                                                                 #
+#   - n.a.                                                                                                                                #
+#                                                                                                                                         #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
 
 
-# Metadata ----------------------------------------------------------------
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# Graphics ----------------------------------------------------------------
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+..GRAPHICS.. <- list(
+    positive.edge.color = "#3F51B5",
+    negative.edge.color = "#F44336"
+)
 
 
 
-POSITIVE.EDGE.COLOR <- "#3F51B5"
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# Errors ------------------------------------------------------------------
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-NEGATIVE.EDGE.COLOR <- "#F44336"
+..ERRORS.. <- list(
+    # Occurrences:
+    #   - when a parent class with generator is instantiated
+    non.instantiable.class = "Class not meant to be instantiable. Use it as an abstract class.",
+
+    # Occurrences:
+    #   - when a type is requested by string (i.e., graph or model alias) and no implementation exists.
+    unsupported.type = "Requested type is not supported. You may request it at: https://github.com/mihaiconstantin/netpaw/issues.",
+
+    # Occurrences:
+    #   - when a object type other than what is expected was provided via the dependency injection
+    incorrect.object.type = "One or more injected arguments are not instances of expected class.",
+
+    # Occurrences:
+    #   - when running into things not yet implemented (e.g., time series models)
+    not.implemented = "Not yet implemented. Most likely it is under development."
+
+    # Occurrences:
+    #   - n.a.
+)
