cd534691eaff1022eda77c684bf609136434353f
mihaiconstantin <c.tinmihai@ymail.com>
2019-06-02 22:37:50 +0200

    Added user feedback to functions in `simulation.R`

--- R/006_simulation.R
+++ R/006_simulation.R
@@ -2,29 +2,32 @@
 
 
 # Run a single design cell (not available to the end users).
-run.cell <- function(sample.size, model.type, graph.type, nodes, ...) {
+run.cell <- function(sample.size, model.type, graph.type, nodes, ..., verbose = TRUE) {
     # Start the timer time.
     start = proc.time()
     
-    # User feedback at the before execution.
-    cat(
-        # The title.
-        crayon::black$bgYellow$bold("Cell config:"),         "\n",
-        
-        # The obvious arguments.
-        "  - sample size: ", crayon::yellow(sample.size),    "\n",
-        "  - model: ", crayon::yellow(model.type),           "\n",
-        "  - graph: ", crayon::yellow(graph.type),           "\n",
-        "  - nodes: ", crayon::yellow(nodes),                "\n",
+    # Should we show user feedback?
+    if(verbose) {
+        # User feedback at the before execution.
+        cat(
+            # The title.
+            crayon::black$bgYellow$bold("Cell config:"),         "\n",
+            
+            # The obvious arguments.
+            "  - sample size: ", crayon::yellow(sample.size),    "\n",
+            "  - model: ", crayon::yellow(model.type),           "\n",
+            "  - graph: ", crayon::yellow(graph.type),           "\n",
+            "  - nodes: ", crayon::yellow(nodes),                "\n",
 
-        # The not so obvious arguments.
-        paste(paste("  - ", gsub("\\.", " ", names(unlist(list(...)))), sep = ""), crayon::yellow(list(...)), sep = ": ", collapse = "\n"),  "\n",
+            # The not so obvious arguments.
+            paste(paste("  - ", gsub("\\.", " ", names(unlist(list(...)))), sep = ""), crayon::yellow(list(...)), sep = ": ", collapse = "\n"),  "\n",
 
-        # `cat` options.
-        sep = ""
-    )
+            # `cat` options.
+            sep = ""
+        )
+    }
 
-	# Select the true model.
+    # Select the true model.
     model <- gen.model(model.type, graph.type, nodes, ...)
     
 	# Sample data based on the true model.
@@ -36,11 +39,18 @@ run.cell <- function(sample.size, model.type, graph.type, nodes, ...) {
     # Prepare the cell results (i.e., of class `npcell`).
     result <- extract.results(model, fit)
 
-    # Stop the timer time.
-    duration = (proc.time() - start)[[3]]
+    # Append the duration.
+    result$time = list(
+        system = Sys.time(),
+        # Stop the timer time.
+        duration = (proc.time() - start)[[3]]
+    )
 
-    # User feedback after execution.
-    cat("  - execution time: ", crayon::green(paste(round(duration, 3), "s", sep = "")), "\n", sep = "")
+    # Should we show user feedback?
+    if(verbose) {
+        # User feedback after execution.
+        cat("  - execution time: ", crayon::green(paste(round(result$time$duration, 3), "s", sep = "")), "\n", sep = "")
+    }
 
     return(result)
 }
@@ -52,40 +62,56 @@ run.cell <- function(sample.size, model.type, graph.type, nodes, ...) {
 #' @export
 run.design <- function(design) {
 	# User feedback at start.
-	cat('-> Running simulation for', crayon::yellow(nrow(design)), 'cells:\n')
+	cat(crayon::white$bgBlue$bold('Simulating', nrow(design$matrix), 'cells:\n\n'))
 
 	# Storing the results per cell.
 	results = list()
 
 	# Running the cells.
-	for (cell in 1:nrow(cells))
+	for (cell.index in 1:nrow(design$matrix))
 	{
-		cell_result = tryCatch(
+		cell.result = tryCatch(
 			{
-				# Try to run the cell.
-				run_cell(cells[cell, 1], cells[cell, 2], cells[cell, 3], cells[cell, 4], cells[cell, 5])
+				# Extract the non-NA cell parameters.
+                config = design$matrix[cell.index, !is.na(design$matrix[1, ])]
+
+                # Remove the prefixes for graph and model options.
+                names(config) <- gsub(paste("graph.options.", config$graph, ".", sep = ""), "", names(config))
+                names(config) <- gsub(paste("model.options.", config$model, ".", sep = ""), "", names(config))
+                
+                # Drop the last column because it indicates the number of replications.
+                config <- config[-length(config)]
+                
+                # Try to run the cell.
+                result <- do.call("run.cell", config)
+
+                # Provide feedback about the success:
+                cat("  - status: ", crayon::green("succeeded"), "\n\n")
+
+                # Return the results.
+                return(result)
 			}, 
 			error = function(error) {
-				# Provide feedback in the console.
-				cat('Cell error. Skipped. \n')
-
-				# Inform about the error (i.e., for logging.)
-				info = paste0('Simulation error at index ', cell, ' in run_cells(). Cell config: ', cells[cell, 1], ' ', cells[cell, 2], ' ', cells[cell, 3], ' ', cells[cell, 4], ' ', cells[cell, 5], '.')
-				message(paste0(cat('\t'), 'SIMERR: ', Sys.time(), '. ', info))
-				message(paste0(cat('\t'), 'Actual error: ', error))
+				# Provide feedback about the error.
+                cat("  - status: ", crayon::red("failed"), "\n\n")
+                
+				# Collect and inform about the error (i.e., for logging.)
+				info = paste('Simulation error at design row ', cell.index, ' in `run.design()`. Cell config: ', paste(config, paste("(", names(config), ")", sep = ""), sep = " ", collapse = " | "), '.', sep = "")
+                message(paste(cat('\t'), 'SIMERR: ', Sys.time(), '. ', info))
+				message(paste(cat('\t'), 'Actual error: ', error))
 				
 				# Return the info string to the results object.
 				return(info)
 			}
 		)
-		results[[cell]] = cell_result
+		results[[cell.index]] = cell.result
 	}
 	
 	# Set the class to allow for fancier handling of the nested data structure.
-	class(results) <- c('netPowerCells', 'list')
+	# class(results) <- c('netPowerCells', 'list')
 
 	# User feedback at end.
-	cat('-> Completed all', nrow(cells), 'cells.\n')
+	cat(crayon::white$bgBlue$bold('Completed all', nrow(design$matrix), 'cells:\n'))
 
 	return(results)
 }
@@ -94,7 +120,7 @@ run.design <- function(design) {
 
 #' @title .
 #' @export
-replicate_cells <- function(cells, replications) {
+replicate.design <- function(cells, replications) {
 	# User feedback at start.
 	cat('Design replications requested: ', replications, '.\n', sep = '')
 
