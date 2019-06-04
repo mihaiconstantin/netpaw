4a9eaa31b7b66ae14d6ef083350c5da116c28e9d
mihaiconstantin <c.tinmihai@ymail.com>
2019-06-04 16:59:14 +0200

    Added `ScaleFreeGraph` class

---
+++ R/ScaleFreeGraph.R
@@ -0,0 +1,65 @@
+# # # # #  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                              _                                           #
+#                             | |                                          #
+#                 _ __    ___ | |_  _ __    __ _ __      __                #
+#                | '_ \  / _ \| __|| '_ \  / _` |\ \ /\ / /                #
+#                | | | ||  __/| |_ | |_) || (_| | \ V  V /                 #
+#                |_| |_| \___| \__|| .__/  \__,_|  \_/\_/                  #
+#                                  | |                                     #
+#                                  |_|                                     #
+# # # # #  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                          #
+# File contributors:                                                       #
+#   - M.A Constantin                                                       #
+#                                                                          #
+# File description:                                                        #
+#   - generated from template on 2019-06-04 16:54:33 by M.A Constantin     #
+#   - you may add your description here                                    #
+#                                                                          #
+# Seminal paper:                                                           #
+#   - https://dx.doi.org/10.1126/science.1173299                           #
+#                                                                          #
+# # # # #  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Generating ScaleFree model parameters ------------------------------------
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+ScaleFreeGraph <- R6::R6Class("ScaleFreeGraph",
+    inherit = Graph,
+
+
+    private = list(
+        ..generator = function(nodes, attachment, edges, directed = FALSE) {
+            # Generate the graph.
+            graph <- as.matrix(igraph::get.adjacency(igraph::sample_pa(n = nodes, power = attachment, m = edges, directed = directed)))
+
+            return(graph)
+        }
+    )
+)
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Alias for ScaleFree graph ------------------------------------------------
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# "ScaleFree" graph is known as "scalefree".
+
+Graph$..ALIASES..$scalefree <- list(
+    name = "scalefree",
+    class = ScaleFreeGraph,
+    example.args = list(
+        nodes = 10,
+        attachment = 2,
+        edges = .5,
+        directed = FALSE
+    )
+)
+
+
+
+# End of file.
