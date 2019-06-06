7d69e1e66795937a3959df2d406ed9344c03403a
mihaiconstantin <c.tinmihai@ymail.com>
2019-06-06 16:07:01 +0200

    Added `includes.R` to constrain the order of collation for parent `R6` classes

---
+++ R/0_includes.R
@@ -0,0 +1,29 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                              _                                          #
+#                             | |                                         #
+#                 _ __    ___ | |_  _ __    __ _ __      __               #
+#                | '_ \  / _ \| __|| '_ \  / _` |\ \ /\ / /               #
+#                | | | ||  __/| |_ | |_) || (_| | \ V  V /                #
+#                |_| |_| \___| \__|| .__/  \__,_|  \_/\_/                 #
+#                                  | |                                    #
+#                                  |_|                                    #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                         #
+# File contributors:                                                      #
+#   - M.A. Constantin                                                     #
+#                                                                         #
+# File description:                                                       #
+#   - specifies the order in which the R6 parent classes must be          #
+#     collated to avoid inheritance issues at build time                  #
+#   - see: http://r-pkgs.had.co.nz/man.html                               #
+#                                                                         #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Classes that are inherited ----------------------------------------------
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+#' @include Graph.R Generator.R CrossSectionalGenerator.R TimeSeriesGenerator.R Estimator.R Sampler.R Comparator.R
+NULL
