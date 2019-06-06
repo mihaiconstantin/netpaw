4d9aca95699e4c86a605b96c8976dc8cd762c727
mihaiconstantin <c.tinmihai@ymail.com>
2019-06-06 10:51:35 +0200

    Updated reading instructions in `zzz.R`

--- R/zzz.R
+++ R/zzz.R
@@ -28,16 +28,10 @@
 # Notes for my future self.
 # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-# The order in which you want to open the files in the `R/` is the following:
-#     1. graph.R -> generates unweighted undirected graphs
-#     2. model.R -> adds parameters to a graph
-#     3. data.R -> generates data based on a parameterized model
-#     4. estimation.R -> fits a model to a give data object
-#     5. outcome.R -> compares a true model and an estimated model and computes several outcome indicators
-#     6. simulation.R -> a wrapper around the files from 1 to 5 that allows to replicate and store results
-#     7. post.R -> allows to post the result of file 5 to whatever API endpoint
-#     8. utils.R -> contains useless functions that I once thought were useful
-#     9. externals.R -> contains code mostly copied---and simplified a bit---from other packages
+# How to read the files in this package?
+#    1. start with the diagram in the `assets/plan/` folder
+#    2. all files that begin with `_` in the `R/` folder are files that contain regular `R` functions
+#    3. all other files (i.e., except this one) are `R6` classes, according to the UML class diagram within `assets/plan/` 
 
 
 
