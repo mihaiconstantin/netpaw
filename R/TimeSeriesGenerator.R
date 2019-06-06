c97b97a09ab2406c9169aa902e4c8f75c7189be3
mihaiconstantin <c.tinmihai@ymail.com>
2019-06-06 08:14:17 +0200

    Added `TimeSeriesGenerator` class according to the UML diagram

---
+++ R/TimeSeriesGenerator.R
@@ -0,0 +1,47 @@
+# # # # #  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                              _                                           #
+#                             | |                                          #
+#                 _ __    ___ | |_  _ __    __ _ __      __                #
+#                | '_ \  / _ \| __|| '_ \  / _` |\ \ /\ / /                #
+#                | | | ||  __/| |_ | |_) || (_| | \ V  V /                 #
+#                |_| |_| \___| \__|| .__/  \__,_|  \_/\_/                  #
+#                                  | |                                     #
+#                                  |_|                                     #
+# # # # #  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                          #
+# File contributors:                                                       #
+#   - M.A. Constantin                                                      #
+#                                                                          #
+# File description:                                                        #
+#   - contains an abstract R6 class that extends the generic Generator     #
+#     defines an abstract time series generator                            #
+#                                                                          #
+# # # # #  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# Specialized time series generator class ---------------------------------
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+TimeSeriesGenerator <- R6::R6Class("TimeSeriesGenerator",
+    inherit = Generator,
+
+
+    private = list(),
+
+
+    public = list(
+        initialize = function(...) {
+            # Call the parent constructor.
+            super$initialize(...)
+        }
+    ),
+
+
+    active = list()
+)
+
+
+
+# End of file.
