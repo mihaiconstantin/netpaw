424f127a744c0415fa8bfbd48f896d6387149320
mihaiconstantin <c.tinmihai@ymail.com>
2019-06-06 10:43:30 +0200

    Added decorators to `utils.R`

--- R/utils.R
+++ R/utils.R
@@ -337,3 +337,55 @@ get.pcor <- function(nvars) {
     
     return(pcor.mat)
 }
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# Decorators --------------------------------------------------------------
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# Definition of the decorator operator.
+`%decorate%` = function(decorator, func) {
+    return(decorator(func))
+}
+
+
+
+# Timer decorator.
+timer.decorator <- function(func) {
+    wrapper <- function(...) {
+        start = proc.time()
+        result <- func(...)
+        duration <- (proc.time() - start)[[3]]
+        cat("Time: ", duration, "s.", sep = "")
+        return(result)
+    }
+    return(wrapper)
+}
+
+
+
+# Decorated `run.cell` function.
+logger.decorator <- function(func) {
+    wrapper <- function(..., use.signature = TRUE, msg.before, msg.after) {
+        
+        # The message before the function is run.
+        cat(msg.before)
+        
+        if(use.signature) {
+            # Capture the `...` arguments.
+            . <- list(...)
+
+            # Print the `...` arguments.
+            cat("- signature:", paste(., crayon::yellow(paste("(", gsub("\\.", " ", names(unlist(.))), ").", sep = "")), collapse = crayon::silver(" | ")))
+        } 
+
+        # Run the function.
+        result <- func(...)
+
+        # The message after the function is run.
+        cat(msg.after)
+        
+        return(result)
+    }
+}
