f198ad489246185b920c12bbd4c61d54c4781121
mihaiconstantin <c.tinmihai@ymail.com>
2019-06-06 16:18:03 +0200

    Updated `Model` to check list length before setting the class fields

--- R/Model.R
+++ R/Model.R
@@ -33,8 +33,8 @@ Model <- R6::R6Class("Model",
         initialize = function(weights, details, list = NULL) {
             if(missing(weights) && missing(details)) {
                 # Set fields from list.
-                self$weights <- list[[1]]
-                self$details <- list[[2]]
+                if(length(list) > 0) self$weights <- list[[1]]
+                if(length(list) > 1) self$details <- list[[2]]
             } else {
                 # Set the fields from values.
                 self$weights <- weights
