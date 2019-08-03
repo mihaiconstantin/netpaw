0897f38950ceb879b03389220cb9bee95a31ce83
mihaiconstantin <c.tinmihai@ymail.com>
2019-08-03 12:11:39 +0200

    Added `Outcome` class

---
+++ R/Outcome.R
@@ -0,0 +1,42 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                              _                                          #
+#                             | |                                         #
+#                 _ __    ___ | |_  _ __    __ _ __      __               #
+#                | '_ \  / _ \| __|| '_ \  / _` |\ \ /\ / /               #
+#                | | | ||  __/| |_ | |_) || (_| | \ V  V /                #
+#                |_| |_| \___| \__|| .__/  \__,_|  \_/\_/                 #
+#                                  | |                                    #
+#                                  |_|                                    #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                         #
+# File contributors:                                                      #
+#   - M.A. Constantin                                                     #
+#                                                                         #
+# File description:                                                       #
+#   - contains a class that defines a model comparison outcome            #
+#                                                                         #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Outcome class -----------------------------------------------------------
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+Outcome <- R6::R6Class("Outcome",
+
+    public = list(
+        values = NULL,
+
+
+        # Constructor.
+        initialize = function(values) {
+            # Set the values from passed values.
+            self$values <- values
+        }
+    )
+)
+
+
+
+# End of file.
