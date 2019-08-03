bb338537c90157b92e601716a29432394f13d49b
mihaiconstantin <c.tinmihai@ymail.com>
2019-08-03 17:23:27 +0200

    Added basic computations for the `Result` class

--- R/Result.R
+++ R/Result.R
@@ -34,7 +34,61 @@ Result <- R6::R6Class("Result",
 
         # This way of computing the simulation result should be applicable to all types of simulations.
         ..compute.basic.result = function() {
-            return("To be added.")
+            # Get the true and estimated edges.
+            true <- private$..true$weights[upper.tri(private$..true$weights)]
+            esti <- private$..estimated$weights[upper.tri(private$..estimated$weights)]
+
+            # Check if perfect recovery in terms of presence/ absence of an edge.
+            perfect <- all((true == 0) == (esti == 0))
+
+            # Check the size of both graphs (i.e., useful of nodes were dropped due to restamping issues).
+            equal.size <- dim(private$..true$weights)[1] == dim(private$..estimated$weights)[1]
+
+            # True/ false | positive/ negative rates.
+            TP <- sum(true != 0 & esti != 0)
+            FP <- sum(true == 0 & esti != 0)
+            TN <- sum(true == 0 & esti == 0)
+            FN <- sum(true != 0 & esti == 0)
+
+            # Compound indicators based on the rates.
+            sensitivity <- TP / (TP + FN) # Aka power.
+            specificity <- TN / (TN + FP)
+            type.one    <- FP / (FP + TN)
+            type.two    <- FN / (TP + FN)
+
+            # Compute type S error rate.
+            # TODO: type S error.
+            type.s <- "not yet implemented"
+
+            # Compute type M error rate.
+            # TODO: type M error.
+            type.m <- "not yet implemented"
+
+            # Edge weights correlation.
+            correlation <- ifelse(equal.size, cor(true, esti), NA)
+
+            # Density for true and estimated graphs. 
+            density.true = sum(true != 0) / length(true)
+            density.esti = sum(esti != 0) / length(esti)
+
+            # Return as a list.
+            return(list(
+                perfect                 = perfect,
+                true.positive           = TP, 
+                false.positive          = FP, 
+                true.negative           = TN, 
+                false.negative          = FN,
+                sensitivity             = sensitivity,
+                specificity             = specificity,
+                type.one                = type.one,
+                type.two                = type.two,
+                type.s                  = type.s,
+                type.m                  = type.m,
+                edge.correlation        = correlation,
+                density.true.model      = density.true,
+                density.estimated.model = density.esti,
+                equal.size              = equal.size
+            ))
         },
 
 
