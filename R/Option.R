5137efb4cfecf9e2495fb0df1e1b589f4f56ca78
mihaiconstantin <c.tinmihai@ymail.com>
2019-08-03 12:07:27 +0200

    Updated `Option` class with several methods

--- R/Option.R
+++ R/Option.R
@@ -25,18 +25,72 @@
 
 Option <- R6::R6Class("Option",
 
+    private = list(
+        ..values = NULL,
+        ..meta = NULL,
+
+
+        # Try to guess which class has been inherited.
+        ..guess.ancestor = function() {
+            # Get the last words that make up the camel case type.
+            words = strsplit(private$..meta$type, "([a-z])(?=[A-Z])", perl = TRUE)[[1]]
+
+            # Get the last word which represents the inherited class.
+            word = tolower(words[length(words)])
+
+            return(word)
+        }
+    ),
+
+
     public = list(
-        values = NULL,
-        meta = NULL,
-
-
+        # Constructor
         initialize = function(values = NULL, meta = NULL) {
+            # Set fields.
+            private$..values <- self$set.values(values)
+            private$..meta <- self$set.meta(meta)
+        },
+
+
+        # Flatten object to a named list.
+        to.list = function() {
+            # Determine the name.
+            name = private$..guess.ancestor()
+
+            # Prepare the simplified list.
+            simple = list()
+
+            # Append option and meta values.
+            simple[[name]] = private$..values
+            simple[[name]]$timestamp = private$..meta$timestamp
+
+            return(simple)
+        },
+
+
+        # Setters.
+        set.values = function(values) {
+            private$..values <- values
+        },
+
+
+        set.meta = function(meta) {
             # Check types.
             if(!is.null(meta)) assert("Meta" %in% class(meta), ..ERRORS..$incorrect.object.type)
 
-            # Set fields.
-            self$values <- values
-            self$meta <- meta
+            private$..meta <- meta
+        }
+    ),
+
+
+    active = list(
+        values = function() {
+            return(private$..values)
+        },
+
+
+        meta = function() {
+            return(private$..meta)
         }
     )
 )
