b64992f98565b1aeb028feb1c62ebdedb2bedfa5
mihaiconstantin <c.tinmihai@ymail.com>
2019-08-25 13:43:20 +0200

    Updated the `Simulation` class to make use of `SimulationRun`

--- R/Simulation.R
+++ R/Simulation.R
@@ -17,6 +17,9 @@
 #                                                                         #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
+# Includes.
+#' @include SimulationRun.R
+
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
@@ -27,25 +30,53 @@ Simulation <- R6::R6Class("Simulation",
 
     private = list(
         ..config = NULL,
-        ..result = NULL,
-        ..completed = NULL
+        ..hash = NULL,
+        ..replications = NULL,
+        ..runs = list(),
+
+
+        # Boilerplate.
+        ..boot = function(config, replications) {
+            # Type check.
+            assert("Config" %in% class(config), ..ERRORS..$incorrect.object.type)
+
+            # Set the config.
+            private$..config <- config
+
+            # Initialize the replications count.
+            private$..replications <- replications
+
+            # Compute the config hash.
+            private$..hash <- digest::digest(config, algo = "md5")
+        }
     ),
 
 
     public = list(
         # Constructor.
-        initialize = function(config) {
-            # Type check.
-            assert("Config" %in% class(config), ..ERRORS..$incorrect.object.type)
-
-            # Set fields.
-            private$..config <- config
+        initialize = function(config, replications) {
+            # Boot.
+            private$..boot(config, replications)
         },
 
 
-        # Convert a config to a simulation procedure and perform it.
-        perform = function() {
-            # Runs based on a config.
+        # Perform and replicate the simulation.
+        # TODO: Allow to run chunks of replications (i.e., different processes) that can later be glued based on the simulation hash.
+        perform = function(vary.generator = FALSE) {
+            # If no replication ran has been performed, run for the first time. 
+            if(!length(private$..runs)) {
+                # Perform the first replication.
+                private$..runs[[1]] <- SimulationRun$new(private$..config)
+            }
+
+            # Decide on what generator to use (i.e., reuse the first ever created or vary it for each run).
+            if(!vary.generator) { generator <- private$..runs[[1]]$generator } else { generator <- NULL }
+
+            # Run and replicate the remaining.
+            for (i in 2:private$..replications) {
+                # Perform the remainder of the replications.
+                private$..runs[[i]] <- SimulationRun$new(config = private$..config, generator = generator)
+            }
         }
     ),
 
@@ -56,13 +87,29 @@ Simulation <- R6::R6Class("Simulation",
         },
 
 
-        result = function() {
-            return(private$..result)
+        hash = function() {
+            return(private$..hash)
+        },
+
+
+        replications = function() {
+            return(private$..replications)
+        },
+
+
+        runs = function() {
+            return(private$..runs)
         },
 
 
         completed = function() {
-            return(private$..completed)
+            if(length(private$..runs)) {
+                return(sum(sapply(private$..runs, function(run) {
+                    if(length(run)) { return(1) } else { return(0) }
+                })))
+            } else {
+                return(0)
+            }
         }
     )
 )
