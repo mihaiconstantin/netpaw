e01cfb11ed5d3370b1737ab29f6ac4653b294d5d
mihaiconstantin <c.tinmihai@ymail.com>
2019-09-09 08:53:41 +0200

    Added empty test for model comparison

---
+++ tests/testthat/test_model_comparison.R
@@ -0,0 +1,38 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                              _                                          #
+#                             | |                                         #
+#                 _ __    ___ | |_  _ __    __ _ __      __               #
+#                | '_ \  / _ \| __|| '_ \  / _` |\ \ /\ / /               #
+#                | | | ||  __/| |_ | |_) || (_| | \ V  V /                #
+#                |_| |_| \___| \__|| .__/  \__,_|  \_/\_/                 #
+#                                  | |                                    #
+#                                  |_|                                    #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                         #
+# File contributors:                                                      #
+#   - M.A. Constantin                                                     #
+#                                                                         #
+# File description:                                                       #
+#   - unit tests for comparing models                                     #
+#                                                                         #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# Comparator will always get a list in constructor.
+
+
+# Testing that the outputs of newly added models match the expectations of the framework.
+context("Model comparison")
+
+
+
+# Test that all comparators correctly construct outcomes.
+test_that("comparator can construct outcome", {})
+
+
+
+# Test the comparator wrapper.
+test_that("comparator wrapper returns outcome for correct model", {})
+
+
+
+# End of file.
