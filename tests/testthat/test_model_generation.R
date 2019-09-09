a97f586ba0e5833d94ce91a2fb037fa3110f1ca1
mihaiconstantin <c.tinmihai@ymail.com>
2019-09-09 08:52:54 +0200

    Added pragmatic test for model generation

--- tests/testthat/test_model_generation.R
+++ tests/testthat/test_model_generation.R
@@ -1,155 +1,78 @@
-context("True model generation")
-
-
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-# Tests related to the `Model` class --------------------------------------
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                              _                                          #
+#                             | |                                         #
+#                 _ __    ___ | |_  _ __    __ _ __      __               #
+#                | '_ \  / _ \| __|| '_ \  / _` |\ \ /\ / /               #
+#                | | | ||  __/| |_ | |_) || (_| | \ V  V /                #
+#                |_| |_| \___| \__|| .__/  \__,_|  \_/\_/                 #
+#                                  | |                                    #
+#                                  |_|                                    #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                         #
+# File contributors:                                                      #
+#   - M.A. Constantin                                                     #
+#                                                                         #
+# File description:                                                       #
+#   - unit tests for generating true model parameters                     #
+#                                                                         #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-test_that("`Model` class is not instantiable", {
-    # Expectation.
-    expect_error(Model$new(), ..ERRORS..$non.instantiable.class)
-})
+
+
+# Testing that the outputs of newly added models match the expectations of the framework.
+context("Model generation")
 
 
 
-test_that("model aliases inherit from `Model` class", {
-    # Preparation.
-    for(alias in Model$..ALIASES..) {
-        test <- "Model" %in% alias$class$get_inherit()$get_inherit()$classname
+# Test that all generators correctly construct models.
+test_that("generators can construct models", {
+    for(gen in Generator$..ALIASES..) {
+        # Build the arguments.
+        args <- c(type = gen$name, gen$example.args)
 
-        # Expectation.
-        expect_equal(test, TRUE)
-    }
-})
+        # Add a graph if the current generator is a cross-sectional generator.
+        if(gen$class$get_inherit()$classname == "CrossSectionalGenerator") {
+            # Create a graph.
+            graph <- generate.graph(type = "random", nodes = 10, p = .3)
 
+            # Append the graph to the arguments.
+            args <- c(args, graph = graph)
+        }
 
+        # Call the generator.
+        result <- do.call("generate.model", args)
 
-test_that("model aliases have implementation", {
-    # Preparation.
-    for(alias in Model$..ALIASES..) {
-        test <- is.null(alias$class$public_methods$generator)
+        # Expect that the model weights is a matrix.
+        expect_equal(is.matrix(result$model$weights), TRUE)
 
-        # Expectation.
-        expect_equal(test, FALSE)
-    }
-})
+        # Expect that the model details is a list or null.
+        expect_equal(is.list(result$model$details) || is.null(result$model$details), TRUE)
 
+        # Expectations about the cross-sectional models.
+        if(gen$class$get_inherit()$classname == "CrossSectionalGenerator") {
+            # Expect that all cross-sectional generators output symmetrical weights.
+            expect_equal(isSymmetric(result$model$weights), TRUE)
 
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-# Tests related to the `ModelCrossSectional` class ------------------------
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-test_that("`ModelCrossSectional` class is not instantiable", {
-    # Preparation.
-    graph <- do.call(Graph$..ALIASES..[[1]]$class$new, Graph$..ALIASES..[[1]]$example.args)
-
-    # Expectation.
-    expect_error(ModelCrossSectional$new(graph), ..ERRORS..$non.instantiable.class)
-})
-
-
-
-test_that("cross-sectional model aliases return a symmetric matrix with 0 on main diagonal", {
-    # Preparation.
-    for(model.alias in Model$..ALIASES..) {
-        # Check that the model alias is indeed of cross-sectional type.
-        if("ModelCrossSectional" %in% model.alias$class$get_inherit()$classname) {
-            for(graph.alias in Graph$..ALIASES..) {
-                # If present ensure that the `directed` argument is set to `FALSE`.
-                if(!is.null(graph.alias$example.args$directed)) {
-                    graph.alias$example.args$directed <- FALSE
-                }
-
-                # Generate the true graph.
-                graph <- do.call(graph.alias$class$new, graph.alias$example.args)
-
-                # Generate the true model.
-                model <- do.call(model.alias$class$new, c(graph = graph, model.alias$example.args))
-
-                # Expectations.
-                expect_equal(isSymmetric(model$model$weights), TRUE)
-                expect_equal(sum(diag(graph$graph)), 0)
-            }
+            # Expect that all cross-sectional generators output weights with 0 on main diagonal.
+            expect_equal(all(diag(result$model$weights) == 0), TRUE)
         }
     }
 })
 
 
 
-test_that("`gen.model` can start the factory for all cross-sectional model aliases", {
-    # Preparation.
-    for(model.alias in Model$..ALIASES..) {
-        # Check that the model alias is indeed of cross-sectional type.
-        if("ModelCrossSectional" %in% model.alias$class$get_inherit()$classname) {
-            for(graph.alias in Graph$..ALIASES..) {
-                # Generate the true graph.
-                graph <- do.call(graph.alias$class$new, graph.alias$example.args)
+# Test the model generator wrapper.
+test_that("generator wrapper returns correct type", {
+    # Generate a `ggm` model.
+    result <- generate.model(type = "ggm", graph = RandomGraph$new(nodes = 10, p = .1))
 
-                # Generate the true model.
-                factory <- do.call(gen.model, c(model.type = model.alias$name, graph = graph, model.alias$example.args))
+    # Expect that the model is of correct type.
+    expect_equal(result$options$meta$type == "GgmGenerator", TRUE)
 
-                # Expectation.
-                expect_equal(is.matrix(factory$first$model$weights), TRUE)
-            }
-        }
-    }
+    # Generate an unregistered model.
+    expect_error(generate.model("unknown"), ..ERRORS..$unsupported.type)
 })
 
 
 
-test_that("`gen.model` fails for all unsupported cross-sectional model aliases", {
-    # Preparation.
-    for(model.alias in Model$..ALIASES..) {
-        # Check that the model alias is indeed of cross-sectional type.
-        if("ModelCrossSectional" %in% model.alias$class$get_inherit()$classname) {
-            for(graph.alias in Graph$..ALIASES..) {
-                # Generate the true graph.
-                graph <- do.call(graph.alias$class$new, graph.alias$example.args)
-
-                # Expectation.
-                expect_error(do.call(gen.model, c(model.type = "unknown", graph = graph, model.alias$example.args)), ..ERRORS..$unsupported.type)
-            }
-        }
-    }   
-})
-
-
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-# Tests related to the `ModelTimeSeries` class ----------------------------
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-test_that("`ModelTimeSeries` class is not instantiable", {
-    # Preparation.
-    graph <- do.call(Graph$..ALIASES..[[1]]$class$new, Graph$..ALIASES..[[1]]$example.args)
-
-    # Expectation.
-    expect_error(ModelTimeSeries$new(graph), ..ERRORS..$not.implemented)
-})
-
-
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-# Very specialized tests for edge cases -----------------------------------
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-test_that("`GgmModel` class `generator` matches `bootnet::genGGM`", {
-    # Seed number.
-    seed <- 1993
-
-    # Generate via `bootnet`.
-    set.seed(seed)
-    model.bootnet.random <- bootnet::genGGM(10, graph = "random", p = .3, propPositive = .5)
-    model.bootnet.smallworld <- bootnet::genGGM(10, graph = "smallworld", nei = 2, p = .3, propPositive = .5)
-    
-    # Generate via `netpaw`.
-    set.seed(seed)
-    model.netpaw.random <- GgmModel$new(graph = RandomGraph$new(10, .3), positive.edge.ratio = .5)
-    model.netpaw.smallworld <- GgmModel$new(graph = SmallWorldGraph$new(10, 2, .3), positive.edge.ratio = .5)
-    
-    # Expectations.
-    expect_equal(model.bootnet.random, model.netpaw.random$model$weights)
-    expect_equal(model.bootnet.smallworld, model.netpaw.smallworld$model$weights)
-})
+# End of file.
