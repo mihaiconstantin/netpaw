1f63034df2179ab3aff85fc63f019c56fdaf84fa
mihaiconstantin <c.tinmihai@ymail.com>
2019-09-09 08:53:30 +0200

    Added empty test for data generation

---
+++ tests/testthat/test_data_generation.R
@@ -0,0 +1,37 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                              _                                          #
+#                             | |                                         #
+#                 _ __    ___ | |_  _ __    __ _ __      __               #
+#                | '_ \  / _ \| __|| '_ \  / _` |\ \ /\ / /               #
+#                | | | ||  __/| |_ | |_) || (_| | \ V  V /                #
+#                |_| |_| \___| \__|| .__/  \__,_|  \_/\_/                 #
+#                                  | |                                    #
+#                                  |_|                                    #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                         #
+# File contributors:                                                      #
+#   - M.A. Constantin                                                     #
+#                                                                         #
+# File description:                                                       #
+#   - unit tests for generating data                                      #
+#                                                                         #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+
+# Testing that the outputs of newly added models match the expectations of the framework.
+context("Data generation")
+
+
+
+# Test that all samplers correctly construct data.
+test_that("sampler can construct data", {})
+
+
+
+# Test the sampler wrapper.
+test_that("sampler wrapper returns data for correct model", {})
+
+
+
+# End of file.
